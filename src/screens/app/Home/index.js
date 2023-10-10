d5b97d4ec02e876e1ecfe94cf7bdb4cdae211692
Leanne Holmes <lholmes24@my.bcit.ca>
2023-10-10 12:53:47 -0700

    Finished status cards and Home screen

--- src/screens/app/Home/index.js
+++ src/screens/app/Home/index.js
@@ -6,6 +6,7 @@ import {
   Text,
   TouchableOpacity,
   View,
+  Image,
 } from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import Header from '../../../components/Header';
@@ -14,8 +15,10 @@ import Title from '../../../components/Title';
 import {setTasks} from '../../../store/tasks';
 import styles from './styles';
 import moment from 'moment';
+import StatusCard from '../../../components/StatusCard';
 
 const Home = ({navigation}) => {
+  const [counts, setCounts] = useState({});
   const tasks = useSelector(state => state.tasks.data);
   const user = useSelector(state => state.user.data);
   const toUpdate = useSelector(state => state.tasks.toUpdate);
@@ -40,12 +43,60 @@ const Home = ({navigation}) => {
       });
   }, [user, toUpdate, dispatch]);
 
+  useEffect(() => {
+    if (tasks?.length) {
+      const highPriority = tasks?.filter(
+        task =>
+          (task?.category === 'urgent' && task?.checked === 'false') ||
+          (task?.category === 'important' && task?.checked === 'false'),
+      );
+      const today = moment(new Date()).format('YYYY-MM-DD');
+      const overdue = tasks?.filter(task => {
+        const deadline = task?.deadline?.seconds * 1000;
+        const deadlineFormatted = moment(deadline).format('YYYY-MM-DD');
+        return moment(deadlineFormatted).isBefore(today);
+      });
+      const quickWin = tasks?.filter(
+        task => task?.category === 'quick_task' && task?.checked === 'false',
+      );
+
+      setCounts({
+        highPriority: highPriority?.length,
+        overdue: overdue?.length,
+        quickWin: quickWin?.length,
+      });
+    }
+  }, [tasks]);
+
   return (
     <SafeAreaView style={styles.container}>
       <Header title="Home" />
 
       <ScrollView>
-        <Title type="thin">Daily Tasks:</Title>
+        <Title type="thin">Tasks Overview:</Title>
+        <View style={styles.row}>
+          <StatusCard label="High Priority" count={counts?.highPriority} />
+          <StatusCard type="error" label="Past Due" count={counts?.overdue} />
+          <StatusCard label="Quick Wins" count={counts?.quickWin} />
+        </View>
+
+        <View style={styles.wrapper}>
+          <TouchableOpacity
+            style={styles.box}
+            onPress={() => navigation.navigate('Tasks')}>
+            <View style={styles.boxRow}>
+              <Text style={styles.title}>Check all my tasks</Text>
+              <Image
+                style={styles.icon}
+                source={require('../../../assets/rightArrow.png')}
+              />
+            </View>
+            <Text style={styles.subtitle}>
+              See all tasks and filter them by categories you have selected when
+              creating them
+            </Text>
+          </TouchableOpacity>
+        </View>
       </ScrollView>
 
       <PlusIcon />
--- src/screens/app/Home/styles.js
+++ src/screens/app/Home/styles.js
@@ -3,6 +3,40 @@ import colors from '../../../constants/colors';
 
 const styles = StyleSheet.create({
   container: {flex: 1},
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 24,
+    marginTop: 12,
+  },
+  wrapper: {
+    width: '95%',
+  },
+  box: {
+    backgroundColor: colors.lightGrey,
+    borderRadius: 15,
+    padding: 22,
+    marginHorizontal: 24,
+    marginVertical: 72,
+  },
+  title: {
+    color: colors.purple,
+    fontSize: 16,
+  },
+  subtitle: {
+    color: 'rgba(64,53,114,0.5)',
+    fontSize: 12,
+    marginTop: 8,
+  },
+  boxRow: {
+    flexDirection: 'row',
+  },
+  icon: {
+    width: 25,
+    height: 25,
+    marginLeft: 10,
+    marginTop: -1,
+  },
 });
 
 export default styles;
